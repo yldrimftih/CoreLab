53c8201e7fb072a522da39428bbfa3bfb52f1060
Fatih Yıldırım <36747348+yldrimftih@users.noreply.github.com>
2025-01-25 01:20:12 +0300

    Variables name were editted.

--- OFDM/ofdm.m
+++ OFDM/ofdm.m
@@ -1,56 +1,56 @@
-% This example prepends a cyclic prefix to OFDM-modulated 16-QAM data. 
-% To be effective for equalization the cyclic prefix (CP) length must equal or exceed the channel length. 
+% This example prepends a cyclic prefix to OFDM-modulated 16-QAM data.
+% To be effective for equalization the cyclic prefix (CP) length must equal or exceed the channel length.
 
 clear; close; clc;
 % Define variables for QAM and OFDM processing. Generate symbols, QAM-modulate, OFDM-modulate, and then add a CP to the signal.
 %  Multiple OFDM symbols can be processed simultaneously and then serialized.
 
-bps = 4;    % Number of bits per symbol 
-M = 2^bps;  % Modulation order
-nFFT = 128; % Number of FFT bins
-nCP = 8;    % CP length
+ModulationOrder      = 4;    % Number of bits per symbol
+NumberOfSymbols      = 2^ModulationOrder;  % Modulation order
+OFDMSubcarrierNumber = 128; % Number of FFT bins
+CPLength             = 8;    % CP length
 
-txsymbols = randi([0 M-1],nFFT,1);
-txgrid = qammod(txsymbols,M,UnitAveragePower=true);
-txout = ifft(txgrid,nFFT);
-% To process multiple symbols, vectorize the txout matrix
-txout = txout(:);
-txcp = txout(nFFT-nCP+1:nFFT);
-txout = [txcp; txout];
+Datas          = randi([0 NumberOfSymbols-1],OFDMSubcarrierNumber,1);
+ModulatedDatas = qammod(Datas,NumberOfSymbols,UnitAveragePower=true);
+OFDMOutData    = ifft(ModulatedDatas,OFDMSubcarrierNumber);
+% To process multiple symbols, vectorize the OFDMOutData matrix
+OFDMOutData    = OFDMOutData(:);
+CPDatas = OFDMOutData(OFDMSubcarrierNumber-CPLength+1:OFDMSubcarrierNumber);
+OFDMOutData = [CPDatas; OFDMOutData];
 
 % Filter the transmission through a channel that adds noise, frequency dependency, and delay to the received signal.
-hchan = [0.4 1 0.4].';
-rxin = awgn(txout,40);       % Add noise   
-rxin = conv(rxin,hchan);     % Add frequency dependency
-channelDelay = dsp.Delay(1); % Could use fractional delay
-rxin = channelDelay(rxin);   % Add delay
+ChannelCoefficients = [0.4 1 0.4].';
+ReceivedSignalsWithAWGN = awgn(OFDMOutData,40);       % Add noise
+ReceivedSignalsWithAWGNAndChannelEffect = conv(ReceivedSignalsWithAWGN,ChannelCoefficients);     % Add frequency dependency
+ChannelDelay = dsp.Delay(1); % Could use fractional delay
+ReceivedSignalAddedChannelDelay = channelDelay(ReceivedSignalsWithAWGNAndChannelEffect);   % Add delay
 
-% Add a random offset less than the CP length. An offset setting of zero models perfect synchronization between transmitted and received signals. 
+% Add a random offset less than the CP length. An offset setting of zero models perfect synchronization between transmitted and received signals.
 % Any timing offset less than the CP length can be compensated by equalization via an additional linear phase.
-offset = randi(nCP) - 1; % random offset less than length of CP
+RandomOffset = randi(CPLength) - 1; % random offset less than length of CP
 
 
 % Remove CP and synchronize the received signal
-rxsync = rxin(nCP+1+channelDelay.Length-offset:end);
-rxgrid = fft(rxsync(1:nFFT),nFFT);
+SyncedReceivedSignal = ReceivedSignalAddedChannelDelay(CPLength+1+channelDelay.Length-RandomOffset:end);
+ReceivedSignaRemovedCP = fft(SyncedReceivedSignal(1:OFDMSubcarrierNumber),OFDMSubcarrierNumber);
 
-% Practical systems require estimation of the channel as part of the signal recovery process. 
-% The combination of OFDM and a CP simplifies equalization to a complex scalar for each frequency bin. 
-% As long as the latency falls within the length of the CP, synchronization is accomplished by the channel estimator. 
+% Practical systems require estimation of the channel as part of the signal recovery process.
+% The combination of OFDM and a CP simplifies equalization to a complex scalar for each frequency bin.
+% As long as the latency falls within the length of the CP, synchronization is accomplished by the channel estimator.
 % A control here allows you to experiment by disabling the equalization at receiver front end. Compare the transmitted signal with the receiver output.
 
 useEqualizer = true;
 if useEqualizer
-    hfchan = fft(hchan,nFFT);
+    ChannelImpulseResponse = fft(ChannelCoefficients,OFDMSubcarrierNumber);
     % Linear phase term related to timing offset
-    offsetf = exp(-1i * 2*pi*offset * (0:nFFT-1).'/nFFT);
-    rxgrideq = rxgrid ./ (hfchan .* offsetf);
+    FrequencyOffset = exp(-1i * 2*pi*RandomOffset * (0:OFDMSubcarrierNumber-1).'/OFDMSubcarrierNumber);
+    OFDMDemodulatedReceivedSignal = ReceivedSignaRemovedCP ./ (ChannelImpulseResponse .* FrequencyOffset);
 else % Without equalization errors occur
-    rxgrideq = rxgrid;
+    OFDMDemodulatedReceivedSignal = ReceivedSignaRemovedCP;
 end
-rxsymbols = qamdemod(rxgrideq,M,UnitAveragePower=true);
+ReceivedDatas = qamdemod(OFDMDemodulatedReceivedSignal,NumberOfSymbols,UnitAveragePower=true);
 
-if max(txsymbols - rxsymbols) < 1e-8
+if max(Datas - ReceivedDatas) < 1e-8
     disp("Receiver output matches transmitter input.");
 else
     disp("Received symbols do not match transmitted symbols.")
