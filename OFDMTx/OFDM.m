63ca2d32f3349f115a0e84f8fb7ff684aaf99be6
Fatih Yildirim <yldrim.ftih@gmail.com>
2025-02-03 00:16:26 +0300

    OFDM Transmitter without the CP

--- OFDMTx/OFDM.m
+++ OFDMTx/OFDM.m
@@ -1,26 +1,37 @@
 clc; clear; close all;
 
 % Parameters
-ModulationOrder  = 4;       % QPSK modulation
-Variance         = 0.01;    % Variance. Since the Signal has unit energy, the variance must be selected between 0:0.01:1
-NumberOfSymbols  = 1000;    % Number of symbols
+ModulationOrder     = 4;       % QPSK modulation
+Variance            = 0.01;    % Variance. Since the Signal has unit energy, the variance must be selected between 0:0.01:1
+NumberOfSymbols     = 1024;    % Number of symbols
+NumberOfSubCarriers = 64;
 
 % Generating Random Data Symbols
 GeneratedDatas   = randi([0 ModulationOrder-1], NumberOfSymbols, 1);
 
 % PSK Modulation
-GeneratedSymbols = pskmod(GeneratedDatas, ModulationOrder, pi/ModulationOrder);
+GeneratedSymbols = pskmod(GeneratedDatas', ModulationOrder, pi/ModulationOrder);
 
-% Generate complex AWGN noise (manual implementation)
+NumberOfOFDMSymbols = NumberOfSymbols / NumberOfSubCarriers;
+SubCarrierSymbols = reshape(GeneratedSymbols,NumberOfSubCarriers, NumberOfOFDMSymbols);
+TimeDomainSymbols = ifft(SubCarrierSymbols);
+
+TransmittedOFDMSymbols = TimeDomainSymbols(:);
+
+plot(real(TransmittedOFDMSymbols));
+hold on;
+plot(imag(TransmittedOFDMSymbols));
+
+% Generate complex AWGN noise 
 NoiseReal        = sqrt(Variance / 2) * randn(NumberOfSymbols, 1); % Real part
 NoiseImaginary   = sqrt(Variance / 2) * randn(NumberOfSymbols, 1); % Imaginary part
 AWGNNoise        = NoiseReal + 1j * NoiseImaginary; % Complex noise
 
 % Add noise to the signal
-ReceivedSymbols  = GeneratedSymbols + AWGNNoise;
+%ReceivedSymbols  = GeneratedSymbols + AWGNNoise;
 
-SNRdB            = mean(abs(GeneratedSymbols).^2) / mean(abs(AWGNNoise).^2);
+%SNRdB            = mean(abs(GeneratedSymbols).^2) / mean(abs(AWGNNoise).^2);
 
-scatterplot(ReceivedSymbols)
+%scatterplot(ReceivedSymbols)
 
 
