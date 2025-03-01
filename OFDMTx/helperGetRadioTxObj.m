af8cc39d682f1fb32a95e84e3d9b1401024666eb
yldrimftih <yldrim.ftih@gmail.com>
2025-03-02 01:59:53 +0300

    empty lines removed

--- OFDMTx/helperGetRadioTxObj.m
+++ OFDMTx/helperGetRadioTxObj.m
@@ -1,10 +1,4 @@
 function radio = helperGetRadioTxObj(ofdmTx)
-%helperGetRadioTxObj(OFDMTX) returns the radio system object RADIO, based
-%   on the chosen radio device and radio parameters such as Gain,
-%   CenterFrequency, MasterClockRate, and Interpolation factor from the
-%   radioParameter structure OFDMTX. The function also returns the spectrumAnalyzer
-%   systemobject SPECTRUMANALYZE inorder to view the transmitted waveform
-
 
 radio                    = sdrtx('Pluto');
 radio.BasebandSampleRate = ofdmTx.SampleRate;
